22e137039a9b954d2d14a66b8489ab32aed84a99
Luis Fernando <luisvirissimo789@gmail.com>
2024-09-03 07:48:42 -0400

    adicionado imagens

--- Script/restaurante.py
+++ Script/restaurante.py
@@ -48,6 +48,7 @@ class tkinterApp(tk.Tk):
 		loginCheck = login.get()
 		senhaCheck = senha.get()
 		senhaConfirmCheck = confirmarSenha.get()		
+		tkinterApp.show_frame(self,PaginaPrincipal)
 		if loginCheck == "":
 			messagebox.showerror("Erro", "Login está vazio")
 			return
@@ -216,33 +217,183 @@ class PaginaPrincipal(tk.Frame):
 # entradas, pratos principais, bebidas, bebidas alcoólicas, sobremesas, menu do chef. 
 class Entradas(tk.Frame): 
 	def __init__(self, parent, controller):
+		
+		self.image_folder = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "Imagens") # Caminho atual de aonde as imagens estão.
 		tk.Frame.__init__(self, parent,background='#1e272e')
 		
 		titulo = ttk.Label(self, text ="Restaurante do Ederson", font = LARGEFONT,anchor="center",justify='center',background='#1e272e',foreground='white')		
 		titulo.pack(anchor='center')
+		btVoltar = ttk.Button(self, text ="Voltar",
+		command = lambda : controller.show_frame(PaginaPrincipal))
+		btVoltar.pack(anchor='center')
+		btBuy = ttk.Button(self, text ="Carrinho",
+		command = lambda : controller.show_frame(Buy))
+		btBuy.pack(anchor='center')
+		
+		comidas = tk.Frame(self,background='#1e272e')
+		comidas.pack(side = "top", fill = "both", expand = True,anchor='center',padx= 580,pady=100)
 
-		button1 = ttk.Button(self, text ="Page 1",
-		command = lambda : controller.show_frame(Buy))	
-		button1.pack(anchor='center')
+		#Entradas
+		image_path = os.path.join(self.image_folder, "entrada1.png")
+		img = Image.open(image_path)
+		img.thumbnail((250, 250))
+		photo = ImageTk.PhotoImage(img)
+		imgEntradas = tk.Label(comidas, image=photo, bg="gray", bd=0)
+		imgEntradas.image = photo
+		imgEntradas.grid(row=0,column=0)
+		btEntradas = ttk.Button(comidas, text ="Purê de Batata com Blueberry e Torrada",
+		command = lambda : controller.show_frame(Entradas))
+		btEntradas.grid(row=1,column=0)
 
-		button2 = ttk.Button(self, text ="Page 2",
-		command = lambda : controller.show_frame(PaginaPrincipal))	
-		button2.pack(anchor='center')
+		#Pratos principais
+		image_path = os.path.join(self.image_folder, "entrada2.png")
+		img = Image.open(image_path)
+		img.thumbnail((250, 250))
+		photo = ImageTk.PhotoImage(img)
+		imgPratosPrincipais = tk.Label(comidas, image=photo, bg="gray", bd=0)
+		imgPratosPrincipais.image = photo
+		imgPratosPrincipais.grid(row=0,column=1,padx= 10)
+		btPratosPrincipais = ttk.Button(comidas, text ="Lagosta anã laranja",
+		command = lambda : controller.show_frame(PratosPrincipais))
+		btPratosPrincipais.grid(row=1,column=1)
+		
+		#Bebibas
+		image_path = os.path.join(self.image_folder, "entrada3.png")
+		img = Image.open(image_path)
+		img.thumbnail((250, 250))
+		photo = ImageTk.PhotoImage(img)
+		imgBebibas = tk.Label(comidas, image=photo, bg="gray", bd=0)
+		imgBebibas.image = photo
+		imgBebibas.grid(row=0,column=2)
+		btBebidas = ttk.Button(comidas, text ="Queijo com Queijo e Vermelho",
+		command = lambda : controller.show_frame(Bebidas))
+		btBebidas.grid(row=1,column=2)
+		
+		#Bebibas Alcoolicas
+		image_path = os.path.join(self.image_folder, "entrada4.png")
+		img = Image.open(image_path)
+		img.thumbnail((250, 250))
+		photo = ImageTk.PhotoImage(img)
+		imgBebibasAlcoolicas = tk.Label(comidas, image=photo, bg="gray", bd=0)
+		imgBebibasAlcoolicas.image = photo
+		imgBebibasAlcoolicas.grid(row=3,column=0)
+		btBebidasAlcoolicas = ttk.Button(comidas, text ="Torrada de Tomate e Berinjela",
+		command = lambda : controller.show_frame(BebidasAlcoolicas))
+		btBebidasAlcoolicas.grid(row=4,column=0)
+		
+		#Sobremesas
+		image_path = os.path.join(self.image_folder, "entrada5.png")
+		img = Image.open(image_path)
+		img.thumbnail((250, 250))
+		photo = ImageTk.PhotoImage(img)
+		imgSobremesas = tk.Label(comidas, image=photo, bg="gray", bd=0)
+		imgSobremesas.image = photo
+		imgSobremesas.grid(row=3,column=1,padx= 10)
+		btSobremesas = ttk.Button(comidas, text ="Bomba de Chocolate",
+		command = lambda : controller.show_frame(Sobremesas))
+		btSobremesas.grid(row=4,column=1)
+
+		#Menu do chef
+		image_path = os.path.join(self.image_folder, "entrada6.png")
+		img = Image.open(image_path)
+		img.thumbnail((250, 250))
+		photo = ImageTk.PhotoImage(img)
+		imgChef = tk.Label(comidas, image=photo, bg="gray", bd=0)
+		imgChef.image = photo
+		imgChef.grid(row=3,column=2)
+		btChef = ttk.Button(comidas, text ="Coxinha de Frango",
+		command = lambda : controller.show_frame(MenuDoChef))
+		btChef.grid(row=4,column=2)
 
 class PratosPrincipais(tk.Frame): 
 	def __init__(self, parent, controller):
+		
+		self.image_folder = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "Imagens") # Caminho atual de aonde as imagens estão.
 		tk.Frame.__init__(self, parent,background='#1e272e')
 		
 		titulo = ttk.Label(self, text ="Restaurante do Ederson", font = LARGEFONT,anchor="center",justify='center',background='#1e272e',foreground='white')		
 		titulo.pack(anchor='center')
+		btVoltar = ttk.Button(self, text ="Voltar",
+		command = lambda : controller.show_frame(PaginaPrincipal))
+		btVoltar.pack(anchor='center')
+		btBuy = ttk.Button(self, text ="Carrinho",
+		command = lambda : controller.show_frame(Buy))
+		btBuy.pack(anchor='center')
+		
+		comidas = tk.Frame(self,background='#1e272e')
+		comidas.pack(side = "top", fill = "both", expand = True,anchor='center',padx= 580,pady=100)
 
-		button1 = ttk.Button(self, text ="Page 1",
-		command = lambda : controller.show_frame(Buy))	
-		button1.pack(anchor='center')
+		#Entradas
+		image_path = os.path.join(self.image_folder, "pratoprin1.png")
+		img = Image.open(image_path)
+		img.thumbnail((250, 250))
+		photo = ImageTk.PhotoImage(img)
+		imgEntradas = tk.Label(comidas, image=photo, bg="#1e272e", bd=0)
+		imgEntradas.image = photo
+		imgEntradas.grid(row=0,column=0)
+		btEntradas = ttk.Button(comidas, text ="Ovo Milenar",
+		command = lambda : controller.show_frame(Entradas))
+		btEntradas.grid(row=1,column=0)
 
-		button2 = ttk.Button(self, text ="Page 2",
-		command = lambda : controller.show_frame(PaginaPrincipal))	
-		button2.pack(anchor='center')
+		#Pratos principais
+		image_path = os.path.join(self.image_folder, "pratoprin2.png")
+		img = Image.open(image_path)
+		img.thumbnail((250, 250))
+		photo = ImageTk.PhotoImage(img)
+		imgPratosPrincipais = tk.Label(comidas, image=photo, bg="#1e272e", bd=0)
+		imgPratosPrincipais.image = photo
+		imgPratosPrincipais.grid(row=0,column=1,padx= 10)
+		btPratosPrincipais = ttk.Button(comidas, text ="Banana de Pizza",
+		command = lambda : controller.show_frame(PratosPrincipais))
+		btPratosPrincipais.grid(row=1,column=1)
+		
+		#Bebibas
+		image_path = os.path.join(self.image_folder, "pratoprin3.png")
+		img = Image.open(image_path)
+		img.thumbnail((250, 250))
+		photo = ImageTk.PhotoImage(img)
+		imgBebibas = tk.Label(comidas, image=photo, bg="#1e272e", bd=0)
+		imgBebibas.image = photo
+		imgBebibas.grid(row=0,column=2)
+		btBebidas = ttk.Button(comidas, text ="Pizzamburguer",
+		command = lambda : controller.show_frame(Bebidas))
+		btBebidas.grid(row=1,column=2)
+		
+		#Bebibas Alcoolicas
+		image_path = os.path.join(self.image_folder, "pratoprin4.png")
+		img = Image.open(image_path)
+		img.thumbnail((250, 250))
+		photo = ImageTk.PhotoImage(img)
+		imgBebibasAlcoolicas = tk.Label(comidas, image=photo, bg="#1e272e", bd=0)
+		imgBebibasAlcoolicas.image = photo
+		imgBebibasAlcoolicas.grid(row=3,column=0)
+		btBebidasAlcoolicas = ttk.Button(comidas, text ="Peixe extremamente venenoso",
+		command = lambda : controller.show_frame(BebidasAlcoolicas))
+		btBebidasAlcoolicas.grid(row=4,column=0)
+		
+		#Sobremesas
+		image_path = os.path.join(self.image_folder, "pratoprin5.png")
+		img = Image.open(image_path)
+		img.thumbnail((250, 250))
+		photo = ImageTk.PhotoImage(img)
+		imgSobremesas = tk.Label(comidas, image=photo, bg="#1e272e", bd=0)
+		imgSobremesas.image = photo
+		imgSobremesas.grid(row=3,column=1,padx= 10)
+		btSobremesas = ttk.Button(comidas, text ="Lula Crua",
+		command = lambda : controller.show_frame(Sobremesas))
+		btSobremesas.grid(row=4,column=1)
+
+		#Menu do chef
+		image_path = os.path.join(self.image_folder, "pratoprin6.png")
+		img = Image.open(image_path)
+		img.thumbnail((250, 250))
+		photo = ImageTk.PhotoImage(img)
+		imgChef = tk.Label(comidas, image=photo, bg="#1e272e", bd=0)
+		imgChef.image = photo
+		imgChef.grid(row=3,column=2)
+		btChef = ttk.Button(comidas, text ="Lasagna",
+		command = lambda : controller.show_frame(MenuDoChef))
+		btChef.grid(row=4,column=2)
 
 class Bebidas(tk.Frame): 
 	def __init__(self, parent, controller):
